88f11f38bfa13db4244af01cc244841883227bd0
Yonsm <Yonsm@qq.com>
2022-08-04 00:12:54 +0800

    refine fan

--- custom_components/zhimijia/fan.py
+++ custom_components/zhimijia/fan.py
@@ -1,5 +1,4 @@
-
-from .zhimi_fan_v3 import *
+from importlib import import_module
 from ..zhimi.entity import ZhiMiEntity, ZHIMI_SCHEMA
 from homeassistant.components.fan import FanEntity, PLATFORM_SCHEMA, DIRECTION_REVERSE, DIRECTION_FORWARD, SUPPORT_PRESET_MODE, SUPPORT_PRESET_MODE, SUPPORT_DIRECTION, SUPPORT_OSCILLATE
 from homeassistant.const import STATE_OFF, STATE_ON
@@ -9,6 +8,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(ZHIMI_SCHEMA)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    module = import_module('.' + config.get('model', 'zhimi.fan.v3').replace('.', '_'), __package__)
+    globals().update({x: getattr(module, x) for x in module.__dict__ if not x.startswith('_')})
     async_add_entities([ZhiMiFan(config)], True)
 
 
@@ -21,21 +22,17 @@ class ZhiMiFan(ZhiMiEntity, FanEntity):
     def supported_features(self):
         return SUPPORT_PRESET_MODE | SUPPORT_OSCILLATE | SUPPORT_DIRECTION
 
-    async def async_poll(self):
-        data = await super().async_poll()
-        return data
-
     @property
     def state(self):
         return (STATE_OFF, STATE_ON)[self.data[Fan.Switch_Status]]
 
     @property
     def preset_modes(self):
-        return ['档位' + str(i) for i in range(Fan_Fan_Level.MIN, Fan_Fan_Level.MAX + 1)]
+        return ['档位' + str(i) for i in range(Fan_Level.MIN, Fan_Level.MAX + 1)]
 
     @property
     def preset_mode(self):
-        return '档位' + str(self.data[Fan.Fan_Level])
+        return '档位' + str(self.data[Fan.Level])
 
     @property
     def oscillating(self):
@@ -43,7 +40,7 @@ class ZhiMiFan(ZhiMiEntity, FanEntity):
 
     @property
     def current_direction(self):
-        return DIRECTION_FORWARD if self.data[Fan.Mode] == Fan_Mode.Straight_Wind else DIRECTION_REVERSE
+        return DIRECTION_REVERSE if self.data.get(Fan.Mode) == Fan_Mode.Natural_Wind else DIRECTION_FORWARD
 
     async def async_turn_on(self, speed, **kwargs):
         await self.async_control(Fan.Switch_Status, True)
@@ -52,10 +49,10 @@ class ZhiMiFan(ZhiMiEntity, FanEntity):
         await self.async_control(Fan.Switch_Status, False)
 
     async def async_set_preset_mode(self, preset_mode):
-        await self.async_control(Fan.Fan_Level, preset_mode[2] if len(preset_mode) > 2 else preset_mode)
+        await self.async_control(Fan.Level, preset_mode[2] if len(preset_mode) > 2 else preset_mode)
 
     async def async_oscillate(self, oscillating):
         await self.async_control(Fan.Horizontal_Swing, oscillating)
 
     async def async_set_direction(self, direction):
-        await self.async_control(Fan.Mode, Fan_Mode.Straight_Wind if direction == DIRECTION_FORWARD else Fan_Mode.Natural_Wind)
+        await self.async_control(Fan.Mode, Fan_Mode.Natural_Wind if direction == DIRECTION_REVERSE else Fan_Mode.Straight_Wind)
